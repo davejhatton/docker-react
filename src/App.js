134763b83e39b1f7a20293c26a5283b25168eab9
davejhatton <davejhatton20@gmail.com>
2020-02-29 14:30:15 +0000

    Change to trigger build

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello worldly goods again</p>
+        <p>Hello world</p>
         <a
           className="App-link"
           href="https://reactjs.org"
